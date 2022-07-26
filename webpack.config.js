c78a0ba7894e97fe751d67567b55ce55f44e23b0
Bahoz <bahozali50@gmail.com>
2022-07-26 13:14:03 +0300

    add logo image and assets folder and move the folder when build the project using copy-webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -12,6 +13,9 @@ module.exports = {
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: './src/index.html',
+    }),
+    new CopyPlugin({
+      patterns: [{ from: './src/assets', to: './assets' }],
     }),
   ],
 
