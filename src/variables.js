a6822d3745f2958d4854998cee811b0255ce977c
Bahoz <bahozali50@gmail.com>
2022-07-27 15:13:02 +0300

    add involvment url api and endpoints

--- src/variables.js
+++ src/variables.js
@@ -1,7 +1,10 @@
-
+const appId = 'CSZHIxGQlLcCAJ4jGdoG';
+const involvementBase =
+  'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi';
 
 export const API = {
-  searchMeals :  'https://www.themealdb.com/api/json/v1/1/search.php?f=k',
-  baseMealUrl: "https://www.themealdb.com/api/json/v1/1"
-}
-
+  searchMeals: 'https://www.themealdb.com/api/json/v1/1/search.php?f=k',
+  baseMealUrl: 'https://www.themealdb.com/api/json/v1/1',
+  endPointLikes: `${involvementBase}/apps/${appId}/likes/`,
+  endPointComments: `${involvementBase}/apps/${appId}/comments/`,
+};
