d020174a1d33294fb314b9b6f55dc6512ac4f0ee
Bahoz <bahozali50@gmail.com>
2022-07-27 16:01:26 +0300

    we have done with setuping the api for adding comment and likes and get comments and likes

--- src/variables.js
+++ src/variables.js
@@ -5,6 +5,6 @@ const involvementBase =
 export const API = {
   searchMeals: 'https://www.themealdb.com/api/json/v1/1/search.php?f=k',
   baseMealUrl: 'https://www.themealdb.com/api/json/v1/1',
-  endPointLikes: `${involvementBase}/apps/${appId}/likes/`,
-  endPointComments: `${involvementBase}/apps/${appId}/comments/`,
+  endPointLikes: `${involvementBase}/apps/${appId}/likes`,
+  endPointComments: `${involvementBase}/apps/${appId}/comments`,
 };
